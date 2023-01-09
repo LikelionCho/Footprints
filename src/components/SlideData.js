ae136297250bcc210ceacc09b4de37197562e575
LikelionCho <dragonac@naver.com>
2023-01-09 10:57:39 +0900

    nonimage

--- src/components/SlideData.js
+++ src/components/SlideData.js
@@ -2,19 +2,13 @@ import jincat from "@assets/jincat.png"
 import camel from "@assets/camel.png"
 import poodle from "@assets/poodle.png"
 import poodle2 from "@assets/poodle2.png"
-import somac from "@assets/somac.png"
-import salgu from "@assets/salgu.png";
+
 
 export const SlideData = [
   {
     image: jincat,
   },
-  {
-    image: somac,
-  },
-  {
-    image: salgu,
-  },
+
   {
     image: poodle,
   },
